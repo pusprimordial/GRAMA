9958ec986d150c1d47b7b24ca6dc01300df191fe
pusprimordial <pedroeacompanhiadochavoador@gmail.com>
2024-03-20 13:55:05 -0300

    0.1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+var canvas;
+var ctx;
+function init() {
+    var canvas = document.getElementById("mainCanvas");
+    ctx = canvas.getContext("2d");
+
+    //tamanho do canvas
+    
+
+    //carrega a textura do mapa
+    const textureImage = new Image();
+    textureImage.src = '../assets/textures.png';
+
+     
+}
+
+
+
