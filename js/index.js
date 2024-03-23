1b321b55c600ba47ea1328650a3735d4f00f0b6a
pusprimordial <pedroeacompanhiadochavoador@gmail.com>
2024-03-23 16:01:33 -0300

    key event to move object

--- js/index.js
+++ js/index.js
@@ -1,18 +1,52 @@
 var canvas;
 var ctx;
-function init() {
-    var canvas = document.getElementById("mainCanvas");
-    ctx = canvas.getContext("2d");
 
-    //tamanho do canvas
+function init() {
+
+    canvas = document.getElementById("mainCanvas");
+
+    var player = {
+        y: 20,
+        x: 20,
+        rectWidth: 40,
+        rectHeight: 40,
+        speed: 5
+    };
+
+    ctx = canvas.getContext("2d");
+    ctx.fillStyle = "gray";
     
 
-    //carrega a textura do mapa
-    const textureImage = new Image();
-    textureImage.src = '../assets/textures.png';
+    drawPlayer(player);
 
-     
+    //evento de tecla pressionada
+    window.addEventListener("keydown", function(event) {
+        //verifica qual tecla foi pressionada
+        switch (event.key) {
+            case "w":
+                player.y -= player.speed;
+                break;
+            case "s":
+                player.y += player.speed;
+                break;
+            case "a":
+                player.x -= player.speed;
+                break;
+            case "d":
+                player.x += player.speed;
+                break;
+        }
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawPlayer(player);
+    });
+
 }
+
+function drawPlayer(player) {
+    ctx.fillRect(player.x, player.y, player.rectWidth, player.rectHeight);
+}
+
+init();
 
 
 
